f139ebb61938266119292dfbc97744f820cc2beb
hassan kiwan <hassanhitleap1@gmail.com>
2021-01-20 16:55:25 +0200

    1/20/2021

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import {NavBar} from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
